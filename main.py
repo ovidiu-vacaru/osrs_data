22b8725b3a0571587b2ae4c504398a5c7e9c158d
Ovi <bob@example.com>
2021-03-05 21:18:06 +0000

    test

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ ge = GrandExchange()
 
 
 top100 = ge.display_top_twenty("2")
+print(top100)
+
 ge.populatedb_items(top100)
 items = ge.extract_items()
+
 objs = []
 for item in items:
     objs.append(item[1])
 ge.populatedb_values(objs)
+
 
 
